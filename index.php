ca123435eccdb39b3de88884299ad5c789e6f06b
lfelipeqn <lfelipeqn@lfelipeqn-N53SM>
2016-06-29 23:42:44 -0500

    -Elaboración Inicial del landingpage para Coljoyas. -Se realiza el ajuste del diseño y se crean los componentes gráficos necesarios.

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+        <script type=text/javascript" src="plugins/bootstrap/js/bootstrap.min.js"></script>
+        <script type=text/javascript" src="js/jquery.js"></script>
+	<title>Coljoyas proximamente...</title>
+    </head>
+    <body>
+        <section id="banner-top">
+            <img class="img-responsive" src="images/coljoyas-negro2.png" />
+            <div class="joya"><div/>
+        </section>
+        <section id="que-hacemos">
+            <div class="row"><p>Qu&eacute; hacemos</p></div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                    <p><img src="images/personas.png" /></p>
+                    <div class="desc">Los joyeros m&aacute;s destacados del pa&iacute;s te traen anillos, accesorios, collares, pulseras y mucho m&aacute;s utilizando los mejores materiales de diamante, oro, esmeralda y plata.</div>
+                    <br>
+                    <div class="desc">Encuentra adem&aacute;s relojer&iacute;a original y con garant&iacute;a de los distribuidores oficiales y paga seguro con tu tarjeta de cr&eacute;dito y d&eacute;bito.</div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                    <p><img src="images/grafica.png" /></p>
+                    <div class="desc">Comprar online joyer&iacute;a en Colombia nunca fue t&aacute;n f&aacute;cil. con Coljoyas.com encuentras un lugar seguro que te ofrece toda variedad de joyer&iacute;a hecha en Colombia</div>
+                    <br>
+                    <div class="desc">Inf&oacute;rmate en coljoyas.com sobre las &uacute;ltimas tendencias en joyer&iacute;a y conocimiento de piedras preciosas.</div>
+                </div>
+            </div>
+        </section>
+        <section id="banner-bottom">
+            <img class="img-responsive" src="images/coljoyas-blanco.png" />
+        </section>
+        <section id="insc">
+            <div class="row"><p>Inscr&iacute;bete ya para conocer las ofertas de lanzamiento</p></div>
+            <br>
+            <form action="" method="post" id="news" name="news">
+                <fieldset>
+                    <div class="row"><input type="text" id="nombre" name="nombre" required placeholder="nombres y apellidos"/></div>
+                    <div class="row"><input type="email" id="correo" name="correo" required placeholder="correo electr&oacute;nico"/></div>
+                    <div class="row"><input type="number" id="telefono" name="telefono" size="10" required placeholder="celular"/></div>
+                    <div class="row"><textarea placeholder="comentarios"></textarea></div>
+                    <div class="row"><button type="submit">enviar</button></div>
+                </fieldset>
+            </form>
+        </section>
+        <footer>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <p><img src="images/coljoyas-negro.png" />&#174; 2016 Colombia</p>
+            </div>
+        </footer>
+    </body>
+</html>
+
+
+
